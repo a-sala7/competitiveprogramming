733a6ba2255f5d4c90b34dc00700bdaa1f4b1784
a-sala7 <sovitpower4@gmail.com>
2019-04-02 14:07:08 +0200

    Create CF807-D2-A.cpp

---
+++ codeforces/CF807-D2-A.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int users;
+    cin>>users;
+    int before[users];
+    int after[users];
+    bool change = false;
+    for (int i = 0; i < users; i++){
+        cin>>before[i]>>after[i];
+        if(before[i]!=after[i])
+            change = true;
+    }
+    if(change){
+        cout<<"rated";
+    } else {
+        bool noobExcelled = false;
+        for (int i = 0; i < users-1; i++){
+            if(before[i]<before[i+1])
+                noobExcelled = true;
+        }
+        if(noobExcelled)
+            cout<<"unrated";
+        else
+            cout<<"maybe";
+    }
+}

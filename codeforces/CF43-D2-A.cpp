71c90850dd7cbb5f52cd3079ad732fa112d1c192
a-sala7 <sovitpower4@gmail.com>
2019-04-03 11:48:27 +0200

    Create CF43-D2-A.cpp

---
+++ codeforces/CF43-D2-A.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name1, name2, curr;
+    bool assigned[2] = {false};
+    int goals;
+    cin>>goals;
+    int goals1 = 0;
+    int goals2 = 0;
+    for (int i = 0; i < goals; i++){
+        bool assignmentIteration = false;
+        cin>>curr;
+        if(!assigned[0]){
+            name1 = curr;
+            assigned[0] = true;
+            goals1++;
+            assignmentIteration = true;
+        }
+        if(!assigned[1] && curr!=name1){
+            name2 = curr;
+            assigned[1] = true;
+            goals2++;
+            assignmentIteration = true;
+        }
+        if(assignmentIteration)
+            continue;
+        if(curr == name1)
+            goals1++;
+        else
+            goals2++;
+    }
+    if(goals1>goals2){
+        cout<<name1;
+    } else {
+        cout<<name2;
+    }
+}

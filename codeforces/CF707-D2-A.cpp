98178b60d2b8cfe27a5c5ae186022b8968784ef9
a-sala7 <sovitpower4@gmail.com>
2019-04-03 12:07:21 +0200

    Create CF707-D2-A.cpp

---
+++ codeforces/CF707-D2-A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin>>n>>m;
+    char myMatrix[n][m];
+    bool colors = false;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
+            cin>>myMatrix[i][j];
+            if(myMatrix[i][j] != 'B' && myMatrix[i][j] != 'W' && myMatrix[i][j] != 'G'){
+                colors = true;
+            }
+        }
+    }
+    if(colors)
+        cout<<"#Color";
+    else
+        cout<<"#Black&White";
+}

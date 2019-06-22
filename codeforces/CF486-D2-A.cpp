93330a3fee06123c9f67f7429682785068b0538a
a-sala7 <sovitpower4@gmail.com>
2019-06-22 16:58:50 +0200

    Create CF486-D2-A.cpp

---
+++ codeforces/CF486-D2-A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    bool nIsOdd;
+    long long originalN, n, odds, evens;
+    cin>>originalN;
+    if(originalN%2==0)
+        nIsOdd = false;
+    else
+        nIsOdd = true;
+    n = originalN/2;
+    odds = n*n;
+    evens = n*(n+1);
+    if(nIsOdd){
+        cout<<evens-odds-originalN;
+    } else {
+        cout<<evens-odds;
+    }
+}

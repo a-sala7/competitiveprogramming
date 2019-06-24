f5d350126beedeec8037f24c72e90b2b9b8764bf
a-sala7 <sovitpower4@gmail.com>
2019-06-24 11:18:49 +0200

    Create CF456-D2-A.cpp

---
+++ codeforces/CF456-D2-A.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stack>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    long n;
+    cin>>n;
+    long price[n], quality[n];
+    for(int i = 0; i < n; i++){
+        cin>>price[i]>>quality[i];
+    }
+    bool found = false;
+    for(int i = 0; i < n; i++){
+        if(price[i]!=quality[i]){
+            found = true;
+            break;
+        }
+    }
+    if(found){
+        cout<<"Happy Alex";
+    } else {
+        cout<<"Poor Alex";
+    }
+}

7cec571e2b6847c1789a89929868ea27bf29fb92
a-sala7 <sovitpower4@gmail.com>
2019-06-24 12:36:58 +0200

    Create CF467-D2-A.cpp

---
+++ codeforces/CF467-D2-A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int residents[n], capacity[n];
+    int suitable = 0;
+    for(int i = 0; i < n; i++){
+        cin>>residents[i]>>capacity[i];
+        if(capacity[i]-residents[i] >= 2)
+            suitable++;
+    }
+    cout<<suitable;
+}

50977ecce0c052b8d9ab5b0ddbe3468a2edc1716
a-sala7 <sovitpower4@gmail.com>
2019-06-25 10:33:16 +0200

    Create CF581-D2-A.cpp

---
+++ codeforces/CF581-D2-A.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int red, blue;
+    cin>>red>>blue;
+    int hipDays = 0;
+    int normalDays = 0;
+    int totalSocks = red+blue;
+    while(red>0 && blue>0){
+        hipDays++;
+        red--;
+        blue--;
+        totalSocks-=2;
+    }
+    while(totalSocks >= 2){
+        normalDays++;
+        totalSocks-=2;
+    }
+    cout<<hipDays<<' '<<normalDays;
+}

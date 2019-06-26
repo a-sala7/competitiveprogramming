4b5e6417e140c73446162d660865ecdec1b15d29
a-sala7 <sovitpower4@gmail.com>
2019-06-26 07:17:19 +0200

    Create One-dimensional Japanese Crossword CF721-D2-A.cpp

---
+++ named sols/codeforces/One-dimensional Japanese Crossword CF721-D2-A.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    string s;
+    cin>>n>>s;
+    int intCount = 0;
+    int bCount = 0;
+    string t;
+    for(int i = 0; i < n; i++){
+        if(i==n-1&&s[i]=='B'){
+            intCount++;
+            bCount++;
+            t+=to_string(bCount);
+        } else if(s[i] == 'W'){
+            if(bCount>0){
+                t+=to_string(bCount);
+                t+=' ';
+                intCount++;
+            }
+            bCount = 0;
+        } else {
+            bCount++;
+        }
+    }
+    if(t.size()==0){
+        cout<<intCount;
+    } else {
+        cout<<intCount<<endl<<t;
+    }
+}

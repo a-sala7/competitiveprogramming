5cdb13eec147e26b4edfd4224f62cb6453ee432e
a-sala7 <sovitpower4@gmail.com>
2019-06-26 07:53:23 +0200

    Create CF546-D2-A.cpp

---
+++ codeforces/CF546-D2-A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long cost, had;
+    int nBananas;
+    cin>>cost>>had>>nBananas;
+    long long paid = 0;
+    for(int i = 1; i <= nBananas; i++){
+        paid+=i*cost;
+    }
+    if(paid > had){
+        cout<<paid-had;
+    } else {
+        cout<<0;
+    }
+}

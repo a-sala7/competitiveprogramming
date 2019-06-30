f5edbc2bf7a311cad09512a1e7ecf9abf7fb07ed
a-sala7 <sovitpower4@gmail.com>
2019-06-30 18:38:59 +0200

    .

--- named sols/codeforces/Keyboard CF88-D2-B.cpp
+++ named sols/codeforces/Keyboard CF88-D2-B.cpp
@@ -7,13 +7,15 @@ using namespace std;
 int main()
 {
     //Keyboard CF88-D2-B
-    //unsolved TLE because apparently the problem setter is a sadist retard
-    //I need to create frequency arrays, fucking arrays of vectors of pairs of ints just
-    //to get a half-decent time complexity on this
-    //and if that wasn't enough, for test 51 I also need the solution to be aware of the distances it's trying so it doesn't
-    //try further ones when the closer isn't working. I don't even want to begin thinking about that, I'll probably have to
-    //rewrite the entire solution, rethink my entire approach for that.
-    //Fuck this stupid ass dumbass shit problem setter and his dumbass problem.
+    /*unsolved TLE because apparently the problem setter is a sadist retard.
+      I need to create frequency arrays, fucking arrays of vectors of pairs of ints just
+      to get a half-decent time complexity on this
+      and if that wasn't enough, for test 51 I also need the solution to be aware of the distances it's trying so it doesn't
+      try further ones when the closer isn't working. I don't even want to begin thinking about that, I'll probably have to
+      rewrite the entire solution, rethink my entire approach for that.
+      Fuck this stupid ass dumbass shit problem setter and his dumbass problem.
+
+      lol no never mind I got it*/
     int n,m,x;
     cin>>n>>m>>x;
     char keyboard[n][m];
@@ -33,18 +35,6 @@ int main()
             }
         }
     }
-    /*for(int i = 0; i < 26; i++){
-        if(letters[i].size()>0){
-            cout<<"Can write "<<(char)(i+'a')<<' '<<letters[i].size()<<" time(s) with the following coords: "<<endl;
-        }
-        for(int j = 0; j < ltrFreq[i]; j++){
-            cout<<" ("<<letters[i][j].first<<' '<<letters[i][j].second<<") "<<endl;
-        }
-    }
-    cout<<"Can write "<<nShifts<<" shift(s) with the following coords: "<<endl;
-    for(int i = 0; i < nShifts; i++){
-        cout<<" ("<<shifts[i].first<<' '<<shifts[i].second<<") "<<endl;
-    }*/
     long q;
     string s;
     cin>>q>>s;
@@ -52,7 +42,6 @@ int main()
     bool possible = true;
     for(int i = 0; i < q; i++){
         int currFreq;
-        int currInt; //to use in accessing the character's relevant info in other arrays
         //fails cuz letter is there or is upper and no shifts
         if(s[i] >= 'a' && s[i] <= 'z'){
             currFreq = ltrFreq[s[i]-'a'];
@@ -64,7 +53,6 @@ int main()
             continue;
         } else {
             currFreq = ltrFreq[s[i]+32-'a'];
-            currInt = s[i]+32-'a';
             if(currFreq<1){
                 cout<<-1;
                 possible = false;
@@ -75,25 +63,33 @@ int main()
                 break;
             }
         }
-        bool moving = true;
-        for(int j = 0; j < currFreq; j++){
-            for(int k = 0; k < nShifts; k++){
-                //x of shift number k : shift[k].first
-                //y of shift number k : shift[k].second
-                //x of letter number j: letters[currInt][j].first
-                //y of letter number j: letters[currInt][j].second
-                int xDist = abs(shifts[k].first-letters[currInt][j].first)*abs(shifts[k].first-letters[currInt][j].first);
-                int yDist = abs(shifts[k].second-letters[currInt][j].second)*abs(shifts[k].second-letters[currInt][j].second);
-                if(x*x >= xDist + yDist){
-                    moving = false;
-                    break;
+    }
+    if(possible){
+        bool can[26] = {false};
+        for(int z = 0; z < 26; z++){
+            for(int i = 0; i < ltrFreq[z]; i++){
+                for(int j = 0; j < nShifts; j++){
+                    /*x of shift number j : shift[j].first
+                      y of shift number j : shift[j].second
+                      x of letter number i: letters[z][i].first
+                      y of letter number i: letters[z][i].second*/
+                    int xDist = abs(shifts[j].first-letters[z][i].first)*abs(shifts[j].first-letters[z][i].first);
+                    int yDist = abs(shifts[j].second-letters[z][i].second)*abs(shifts[j].second-letters[z][i].second);
+                    if(x*x >= xDist + yDist){
+                        can[z] = true;
+                    }
                 }
             }
-            if(!moving)
-                break;
         }
-        if(moving)
-            iLikeToMoveItMoveIt++;
+        for(int i = 0; i < q; i++){
+            if(s[i]>='a'&&s[i]<='z'){
+                continue;
+            } else {
+                if(!can[s[i]+32-'a']){
+                    iLikeToMoveItMoveIt++;
+                }
+            }
+        }
     }
     if(possible){
         cout<<iLikeToMoveItMoveIt;

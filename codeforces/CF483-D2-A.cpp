eafcbffd2b0ed9492055ccb441e6b17cd0dfe7c1
a-sala7 <sovitpower4@gmail.com>
2019-07-22 22:21:29 +0200

    .

--- codeforces/CF483-D2-A.cpp
+++ codeforces/CF483-D2-A.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
+#define ull unsigned long long
 
 using namespace std;
 
 int main()
 {
-	//Counterexample CF483-D2-A
-    long long l, r;
+    //Counterexample CF483-D2-A
+    ull l,r,a,b,c;
     cin>>l>>r;
-    if(l%2!=0){
-        l++;
-    }
-    if(r-l <= 1){
+    if(r-l<2)
         cout<<-1;
-    } else {
-        cout<<l<<' '<<l+1<<' '<<l+2;
+    else
+    {
+        if(l%2!=0){
+            a = l+1;
+        } else {
+            a = l;
+        }
+        b = a+1;
+        c = b+1;
+        if(c>r)
+            cout<<-1;
+        else
+            cout<<a<<' '<<b<<' '<<c;
     }
 }

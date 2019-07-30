a29c837a73c9b8f4e0b03c59fe2e74f1b268b232
a-sala7 <sovitpower4@gmail.com>
2019-07-30 14:31:21 +0200

    .

---
+++ named sols/codeforces/Complicated GCD CF664-D2-A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#define ull unsigned long long
+
+using namespace std;
+
+int main()
+{
+    //Complicated GCD CF664-D2-A
+    string a, b;
+    cin>>a>>b;
+    if(a!=b)
+        cout<<1;
+    else
+        cout<<a;
+}

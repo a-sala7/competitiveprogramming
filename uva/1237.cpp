24d6cfb53448b10510bf5b1d486ba2e97ace0f77
a-sala7 <sovitpower4@gmail.com>
2019-08-18 01:22:39 +0200

    .

---
+++ uva/1237.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#define fr first
+#define sc second
+using namespace std;
+
+int main()
+{
+    //Expert Enough 1237
+    int t;
+    cin>>t; //test cases
+    for(int k = 0; k < t; k++){
+        int n; //database size
+        cin>>n;
+        pair<string, pair<long, long>> database[n];
+        string s; //list of names
+        long a, b; //range start-end
+        for(int i = 0; i < n; i++){
+            cin>>s>>a>>b;
+            database[i].fr = s;
+            database[i].sc.fr = a;
+            database[i].sc.sc = b;
+        }
+        int q;
+        cin>>q;
+        long query;
+        int results;
+        string ans;
+        for(int i = 0; i < q; i++){
+            cin>>query;
+            results = 0;
+            for(int j = 0; j < n; j++){
+                if(query >= database[j].sc.fr && query <= database[j].sc.sc){
+                    results++;
+                    ans = database[j].fr;
+                    if(results > 1)
+                        break;
+                }
+            }
+            if(results != 1)
+                cout<<"UNDETERMINED"<<endl;
+            else
+                cout<<ans<<endl;
+        }
+        if(k<t-1){
+            cout<<endl;
+        }
+    }
+}
